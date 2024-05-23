fa31f3c573e98955e822dc2c4d06f006f92dfd68
ayyubnurumi <ayyubinvestment@gmail.com>
2024-05-23 15:39:20 +0700

    vue init commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <teleport to="#modals" v-if="showModal">
+    <!-- eslint-disable-next-line -->
+    <CustomModal :header="header" :text="text" theme="sale" @close="toggleModal">
+      <h2>Ninja Giveways!</h2>
+      <p>Grab ur ninja stuff</p>
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+    </CustomModal>
+  </teleport>
+  <button @click.shift="toggleModal">Show modal (shift)</button>
+
+  <teleport to="#modals" v-if="showModalII">
+    <!-- eslint-disable-next-line -->
+    <CustomModal :header="header" :text="text" @close="toggleModalII"></CustomModal>
+  </teleport>
+  <button @click="toggleModalII">Show modal</button>
+  <br />
+  <br />
+  <input type="text" ref="name" />
+  <button @click="handleClick">click me daddy</button>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CustomModal from "./components/CustomModal.vue";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { CustomModal },
+  data() {
+    return {
+      title: "My first Vue App :)",
+      header: "Sign up for special features!",
+      text: "Grab ur spesial giveaways!",
+      showModal: false,
+      showModalII: false,
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalII() {
+      this.showModalII = !this.showModalII;
+    },
   },
 };
 </script>
 
 <style>
-#app {
+#app,
+#modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/CustomModal.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <slot>
+        <h2>{{ header }}</h2>
+        <p>{{ text }}</p>
+      </slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["header", "text", "theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+
+.modal h2 {
+  color: #03cfb4;
+  border: none;
+  padding: 0;
+}
+
+.modal p {
+  font-style: normal;
+}
+
+.modal .actions {
+  text-align: center;
+  margin: 30px 0 10px 0;
+}
+
+.modal .actions a {
+  color: #333;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+
+.modal.sale h2,
+.modal.sale .actions a {
+  color: white;
+}
+</style>
